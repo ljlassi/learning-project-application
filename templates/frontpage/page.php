012985dc6a9a4a5db3710fa833a2b4b350cc7f71
Lassi Jääskeläinen <lassi@lassijaaskelainen.fi>
2020-06-21 17:54:54 +0300

    Added form for adding product at front page

--- templates/frontpage/page.php
+++ templates/frontpage/page.php
@@ -1,2 +1,11 @@
-<h2>Site under construction</h2>
-<p>Site is badly under construction</p>
+<h2>Add product</h2>
+<form action="#" method="post">
+  <div class="form-group">
+    <label for="product_name">Product name</label>
+    <input type="text" name="product_name" id="product_name" class="form-control">
+  </div>
+  <div class=form-group>
+    <label for="product_price">Product price</label>
+    <input type="text" name="product_price" id="product_price" class="form-control">
+  </div>
+</form>
